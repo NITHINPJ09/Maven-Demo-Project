291c91b8afafe0d1320b75a679a4a7b504c31206
Nithin Prince John <nithinpj09@gmail.com>
2023-04-06 14:46:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "vm_provisioning" {
   local_file_name               = var.local_file_name
 }
 
-module "storing_private_key_storage_account" {
+module "storing_private_key_in_storage_account" {
   source                  = "./modules/storage_account"
   depends_on              = [module.vm_provisioning]
   storage_account_name    = var.storage_account_name
@@ -28,5 +28,3 @@ module "storing_private_key_storage_account" {
   resource_group_location = azurerm_resource_group.app_grp.location
   local_file_name         = var.local_file_name
 }
-
-
