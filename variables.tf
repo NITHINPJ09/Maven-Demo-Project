28f23c4101b7f76cd6f104b98fb07896f710af21
Nithin Prince John <nithinpj09@gmail.com>
2023-04-08 18:36:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,3 @@
-variable "ASI" {
-  sensitive   = true
-  description = "Subscription ID of Azure"
-}
-variable "ACI" {
-  sensitive   = true
-  description = "Client ID"
-}
-variable "ACS" {
-  sensitive   = true
-  description = "Client Secret"
-}
-variable "ATI" {
-  sensitive   = true
-  description = "Tenant ID"
-}
 variable "resource_group_name" {
   description = "Name of the Resource Group"
 }
