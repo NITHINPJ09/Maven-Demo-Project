45b8efe53ca9dccf85380fb7793e749f3ca2c437
Nithin Prince John <nithinpj09@gmail.com>
2023-04-10 15:30:07 +0530

    Update variables.tf

--- modules/storage_account/variables.tf
+++ modules/storage_account/variables.tf
@@ -1,18 +1,24 @@
 variable "storage_account_name" {
+  type        = string
   description = "Name of the Storage Account"
 }
 variable "storage_container_name" {
+  type        = string
   description = "Name of the Storage Container"
 }
 variable "storage_blob_name" {
+  type        = string
   description = "Name of the Storage Blob"
 }
 variable "resource_group_name" {
+  type        = string
   description = "Name of the Resource Group"
 }
 variable "resource_group_location" {
+  type        = string
   description = "Location of the Resource Group"
 }
 variable "local_file_name" {
+  type        = string
   description = "Name of the Local File"
 }
