2ec6779472e1bbfc6383195e16c65f0e50adf39a
Nithin Prince John <nithinpj09@gmail.com>
2023-04-10 12:05:45 +0530

    Update variables.tf

--- modules/virtual_machine/variables.tf
+++ modules/virtual_machine/variables.tf
@@ -1,27 +1,40 @@
 variable "virtual_network_name" {
+  type        = string
   description = "Name of the Virtual Network"
 }
 variable "subnet_name" {
+  type        = string
   description = "Name of the Subnet"
 }
+variable "network_security_group_name" {
+  type        = string
+  description = "Name of the Network Security Group"
+}
 variable "network_interface_name" {
+  type        = string
   description = "Name of the Network Interface"
 }
 variable "virtual_machine_name" {
+  type        = string
   description = "Name of the Virtual Machine"
 }
 variable "username" {
+  type        = string
   description = "Name of the User"
 }
 variable "public_ip_name" {
+  type        = string
   description = "Name of the Public IP"
 }
 variable "resource_group_name" {
+  type        = string
   description = "Name of the Resource Group"
 }
 variable "resource_group_location" {
+  type        = string
   description = "Location of the Resource Group"
 }
 variable "local_file_name" {
+  type        = string
   description = "Name of the Local File"
 }
